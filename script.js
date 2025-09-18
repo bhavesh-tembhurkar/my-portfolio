7e493f3a7047ae2ee761a56af7e04b46d5238c59
Bhavesh Tembhurkar <155955395+bhavesh-tembhurkar@users.noreply.github.com>
2025-09-18 16:33:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Wait for the document to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Select the navigation bar
+    const navbar = document.querySelector('.navbar');
+
+    // Function to run when the user scrolls
+    function handleScroll() {
+        if (window.scrollY > 50) {
+            // If scrolled more than 50px, add the 'scrolled' class
+            navbar.classList.add('scrolled');
+        } else {
+            // Otherwise, remove it
+            navbar.classList.remove('scrolled');
+        }
+    }
+
+    // Listen for the 'scroll' event
+    window.addEventListener('scroll', handleScroll);
+
+});
